6528547b6a9ae5e98cf5446f5a122b4774930646
Oscar Vivas <oscar.23695@udenar.edu.co>
2023-06-22 22:50:37 -0500

    primer sprint

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'semilleros';
+}
